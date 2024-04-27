ff379df82ba71f4fd0a63ce07b11804795130829
nbolinaga <nbolinaga17@gmail.com>
2024-04-27 09:27:53 -0400

    added proxy security

--- index.js
+++ index.js
@@ -3,14 +3,24 @@ import request from 'request';
 import { load } from 'cheerio';
 const app = express();
 
+// Middleware to verify the presence and value of the X-RapidAPI-Proxy-Secret header
+const rapidAPIMiddleware = (req, res, next) => {
+  const rapidAPIProxySecret = req.headers['x-rapidapi-proxy-secret'];
+  if (!rapidAPIProxySecret || rapidAPIProxySecret !== YOUR_RAPIDAPI_PROXY_SECRET) {
+    return res.status(403).json({ error: 'Unauthorized' });
+  }
+  next();
+};
+
+app.use(express.json());
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*'); // Allow any origin
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, X-RapidAPI-Proxy-Secret');
   next();
 });
 
-app.get('/', function (req, res) {
+app.get('/', rapidAPIMiddleware, function (req, res) {
   let url = 'https://www.bcv.org.ve';
 
   request({
